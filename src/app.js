8b6722579d5497da02ee24fc2f904dc7010efd88
JuanjoRuiz <juanjoserusa@gmail.com>
2022-09-29 19:25:53 +0000

    segundo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var inicio = ["www."]
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['puedes','racoon', 'capcom' ];
+var extension =["com", "net", "es", "io"]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//function DomainGenerator(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (i = 0; i < pronoun.length; i++) {
+  for ( j= 0; j < adj.length; j++){
+    for (k = 0; k< noun.length; k++){
+      for(l = 0; l < extension.length; l++){
+
+        console.log(`${inicio}${pronoun[i]}${adj[j]}${noun[k]}.${extension[l]}`)
+
+
+      /*const domain = `${pronoun[i]}${adj[j]}${noun[k]}`;
+      let domainArray = domain.split();
+      let domainArrayReverse = domainArray.reverse();
+      let reverseExtension = extension[l].split().reverse();
+      let cont = 0
+
+        for(w = 0; w < reverseExtension.length; w++){
+          if(domainArrayReverse[w] === reverseExtension[w]){
+            cont++
+          }
+        }
+
+        if (cont === reverseExtension.length){
+          domainArray.slice(-2,-1, '.');
+        }
+console-log(domainArray)*/
+
+
+  
+    }
+  }
+}
+
+}
+
+
+
+
+
+
+
